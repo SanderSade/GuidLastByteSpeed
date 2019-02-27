9311ce30e2b3f0e8be2c8a961059664fb65981bb
Sander Säde <sasde@microsoft.com>
2019-02-27 14:50:32 +0200

    Add project files.

---
+++ GuidHandlingTest/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace GuidHandlingTest
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Validate();
+			var summary = BenchmarkRunner.Run<GuidBenchmark>();
+			Console.Write(summary.AllRuntimes);
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		///     Naive validation of different methods
+		/// </summary>
+		private static void Validate()
+		{
+			var guidBenchmark = new GuidBenchmark();
+			guidBenchmark.Setup();
+			var viaString = guidBenchmark.ViaString();
+			var viaByteArray = guidBenchmark.ViaByteArray();
+			var viaReflection = guidBenchmark.ViaReflection();
+			var viaDelegate = guidBenchmark.ViaDelegate();
+
+			for (var i = 0; i < viaString.Count; i++)
+				if (viaString[i] != viaByteArray[i]
+				    || viaByteArray[i] != viaReflection[i]
+				    || viaReflection[i] != viaDelegate[i])
+					throw new InvalidDataException("Byte unmatch!");
+
+			Console.WriteLine("Validation successful");
+		}
+
+
+		[MemoryDiagnoser]
+		[ClrJob(true)]
+		[RankColumn]
+		public class GuidBenchmark
+		{
+			private const int Count = 10000;
+			private List<Guid> _guids;
+
+			[GlobalSetup]
+			public void Setup()
+			{
+				_guids = new List<Guid>(Count);
+				for (var i = 0; i < Count; i++)
+					_guids.Add(Guid.NewGuid());
+			}
+
+
+			[Benchmark]
+			public List<byte> ViaString()
+			{
+				//after https://stackoverflow.com/a/14335076/3248515
+				byte ParseHex(string hexString)
+				{
+					int ParseNybble(char c)
+					{
+						unchecked
+						{
+							var i = (uint) (c - '0');
+							if (i < 10)
+								return (int) i;
+							i = (c & ~0x20u) - 'A';
+							if (i < 6)
+								return (int) i + 10;
+							throw new ArgumentException("Invalid nybble: " + c);
+						}
+					}
+
+					var high = ParseNybble(hexString[0]);
+					var low = ParseNybble(hexString[1]);
+					return (byte) ((high << 4) | low);
+				}
+
+				var result = new List<byte>(Count);
+				foreach (var guid in _guids)
+					result.Add(ParseHex(guid.ToString("N").Substring(30, 2)));
+
+				return result;
+			}
+
+
+			[Benchmark]
+			public List<byte> ViaByteArray()
+			{
+				var result = new List<byte>(Count);
+				foreach (var guid in _guids)
+					result.Add(guid.ToByteArray()[15]);
+				return result;
+			}
+
+			/// <summary>
+			///     From https://stackoverflow.com/a/16222886/3248515, modified
+			/// </summary>
+			private static Func<S, T> CreateGetter<S, T>()
+			{
+				var field = typeof(Guid).GetField("_k", BindingFlags.NonPublic | BindingFlags.Instance);
+				Debug.Assert(field != null, nameof(field) + " != null");
+				var methodName = field.ReflectedType.FullName + ".get_" + field.Name;
+				var setterMethod = new DynamicMethod(methodName, typeof(T), new Type[1] {typeof(S)}, true);
+				var gen = setterMethod.GetILGenerator();
+				if (field.IsStatic)
+				{
+					gen.Emit(OpCodes.Ldsfld, field);
+				}
+				else
+				{
+					gen.Emit(OpCodes.Ldarg_0);
+					gen.Emit(OpCodes.Ldfld, field);
+				}
+
+				gen.Emit(OpCodes.Ret);
+				return (Func<S, T>) setterMethod.CreateDelegate(typeof(Func<S, T>));
+			}
+
+
+			[Benchmark]
+			public List<byte> ViaDelegate()
+			{
+				var getter = CreateGetter<Guid, byte>();
+
+				var result = new List<byte>(Count);
+				foreach (var guid in _guids)
+					result.Add(getter.Invoke(guid));
+				return result;
+			}
+
+			[Benchmark]
+			public List<byte> ViaReflection()
+			{
+				var field = typeof(Guid).GetField("_k", BindingFlags.NonPublic | BindingFlags.Instance);
+
+				var result = new List<byte>(Count);
+				foreach (var guid in _guids)
+				{
+					Debug.Assert(field != null, nameof(field) + " != null");
+					result.Add((byte) field.GetValue(guid));
+				}
+
+				return result;
+			}
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+### Testing different methods for getting the last byte from GUID from performance perspective
+
+
+``` ini
+
+BenchmarkDotNet=v0.11.4, OS=Windows 10.0.17763.316 (1809/October2018Update/Redstone5)
+Intel Core i7-8650U CPU 1.90GHz (Kaby Lake R), 1 CPU, 4 logical and 4 physical cores
+  [Host] : .NET Framework 4.7.2 (CLR 4.0.30319.42000), 32bit LegacyJIT-v4.7.3324.0
+  Clr    : .NET Framework 4.7.2 (CLR 4.0.30319.42000), 32bit LegacyJIT-v4.7.3324.0
+
+Job=Clr  Runtime=Clr  
+
+```
+|        Method |       Mean |     Error |    StdDev | Ratio | Rank | Gen 0/1k Op | Gen 1/1k Op | Gen 2/1k Op | Allocated Memory/Op |
+|-------------- |-----------:|----------:|----------:|------:|-----:|------------:|------------:|------------:|--------------------:|
+|     ViaString | 2,326.3 us | 46.081 us | 68.972 us |  1.00 |    1 |    238.2813 |           - |           - |           986.38 KB |
+|               |            |           |           |       |      |             |             |             |                     |
+|  ViaByteArray |   220.8 us |  4.815 us |  9.726 us |  1.00 |    1 |     69.0918 |           - |           - |           283.24 KB |
+|               |            |           |           |       |      |             |             |             |                     |
+|   **ViaDelegate** |   168.9 us |  1.919 us |  1.795 us |  1.00 |    1 |      2.4414 |      0.7324 |           - |            10.67 KB |
+|               |            |           |           |       |      |             |             |             |                     |
+| ViaReflection | 1,962.7 us | 22.117 us | 20.688 us |  1.00 |    1 |     85.9375 |           - |           - |           361.39 KB |
